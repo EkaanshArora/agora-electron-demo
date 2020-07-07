a5bd2f4a776a1420360b573443c04b7a8cd2308d
Ekaansh Arora <blue.name68@gmail.com>
2020-07-08 01:30:31 +0530

    initial upload

---
+++ src/render.js
@@ -0,0 +1,35 @@
+const AgoraRtcEngine = require('agora-electron-sdk').default
+const APPID = "5c2412e4b1dd4ac89db273c928e29b4d"; //Enter App  ID here
+
+if (!APPID) {
+  alert('Please enter APPID in src/render.jsx (line:2)');
+}
+
+let rtcEngine = new AgoraRtcEngine();
+rtcEngine.initialize(APPID);
+
+rtcEngine.on('joinedChannel', (channel, uid, elapsed) => {
+  let localVideoContainer = document.querySelector('#local');
+  rtcEngine.setupLocalVideo(localVideoContainer);
+})
+
+rtcEngine.on('userJoined', (uid) => {
+  let remoteVideoContainer = document.querySelector('#remote')
+  rtcEngine.setupViewContentMode(uid, 1);
+  rtcEngine.subscribe(uid, remoteVideoContainer)
+})
+
+rtcEngine.setChannelProfile(0)
+// rtcEngine.setVideoEncoderConfiguration({frameRate:30});
+rtcEngine.enableVideo()
+
+document.getElementById('start').onclick = () => {
+  rtcEngine.joinChannel(null, "electron", null, Math.floor(new Date().getTime() / 1000))
+};
+
+document.getElementById('stop').onclick = () => {
+  rtcEngine.leaveChannel();
+  document.getElementById('local').innerHTML = '';
+  document.getElementById('remote').innerHTML = '';
+  // rtcEngine.release();
+};
